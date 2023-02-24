cd60b5336729db6a1410f2a4ac430581e4a5f7b5
ranishah0402 <ranishah@bu.edu>
2023-02-24 14:33:25 -0500

    added changes to sql schema with more constraints

--- skeleton/schema.sql
+++ skeleton/schema.sql
@@ -13,23 +13,24 @@ DROP TABLE IF EXISTS Users CASCADE;
 
 
 CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
+    user_id int4  NOT NULL AUTO_INCREMENT,
     email varchar(255) UNIQUE,
-    password varchar(255),
-    first_name varchar(225),
-    last_name varchar(225),
-    birthdate varchar(225),
+    password varchar(255) NOT NULL DEFAULT '',
+    first_name varchar(225) NOT NULL DEFAULT '',
+    last_name varchar(225) NOT NULL DEFAULT '',
+    birthdate DATE,
     hometown varchar(225),
+    gender varchar(6),
   CONSTRAINT users_pk PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Album (
 	album_id int4 AUTO_INCREMENT,
-    album_name VARCHAR(225),
-    created_date DATE NOT NULL,
-    user_id int4,
+    album_name VARCHAR(225) NOT NULL DEFAULT '',
+    created_date DATETIME DEFAULT current_timestamp,
+    user_id int4 NOT NULL,
     CONSTRAINT album_pk PRIMARY KEY (album_id),
-    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Pictures
@@ -39,55 +40,59 @@ CREATE TABLE Pictures
   imgdata longblob ,
   caption VARCHAR(255),
   INDEX upid_idx (user_id),
-  album_id int4,
+  album_id int4 NOT NULL,
   CONSTRAINT pictures_pk PRIMARY KEY (picture_id),
   CONSTRAINT user_fk1 FOREIGN KEY (user_id) REFERENCES Users(user_id),
-  CONSTRAINT album_fk FOREIGN KEY (album_id) REFERENCES Album(album_id)
+  CONSTRAINT album_fk FOREIGN KEY (album_id) REFERENCES Album(album_id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE Comments (
-	comment_id int4 AUTO_INCREMENT,
-    comment_text VARCHAR(225),
-    comment_date DATE,
-    user_id int4,
-    photo_id int4,
+	comment_id int4 NOT NULL AUTO_INCREMENT,
+    comment_text VARCHAR(225) NOT NULL,
+    comment_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_id int4 NOT NULL,
+    photo_id int4 NOT NULL,
     CONSTRAINT comment_pk PRIMARY KEY (comment_id),
-    CONSTRAINT user_fk2 FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    CONSTRAINT photo_fk1 FOREIGN KEY (photo_id) REFERENCES Pictures(picture_id)
+    CONSTRAINT user_fk2 FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    CONSTRAINT photo_fk1 FOREIGN KEY (photo_id) REFERENCES Pictures(picture_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Likes (
-	like_id int4 AUTO_INCREMENT,
     user_id int4,
     photo_id int4,
-    CONSTRAINT like_pk PRIMARY KEY (like_id),
-    CONSTRAINT user_fk3 FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    CONSTRAINT photo_fk2 FOREIGN KEY (photo_id) REFERENCES Pictures(picture_id) 
+    CONSTRAINT like_pk PRIMARY KEY (photo_id, user_id),
+    CONSTRAINT user_fk3 FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    CONSTRAINT photo_fk2 FOREIGN KEY (photo_id) REFERENCES Pictures(picture_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Tag(
+	tag_id int4 auto_increment,
 	tag_word VARCHAR(225),
-    CONSTRAINT word_pk PRIMARY KEY (tag_word)
+    CONSTRAINT tag_pk PRIMARY KEY (tag_id)
 );
 
 CREATE TABLE assigned_tag(
 	photo_id int4,
-    tag_word VARCHAR(225),
-    CONSTRAINT photo_tag_pk PRIMARY KEY (photo_id, tag_word),
+    tag_id int4,
+    CONSTRAINT photo_tag_pk PRIMARY KEY (photo_id, tag_id),
     CONSTRAINT photo_fk3 FOREIGN KEY (photo_id) REFERENCES Pictures(picture_id),
-	CONSTRAINT tag_fk FOREIGN KEY (tag_word) REFERENCES Tag(tag_word)
+	CONSTRAINT tag_fk FOREIGN KEY (tag_id) REFERENCES Tag(tag_id)
 
 );
 
 CREATE TABLE friends_with(
 	user_id int4,
     friend_id int4,
+    CHECK (user_id <> friend_id),
     CONSTRAINT friends_pk PRIMARY KEY (user_id, friend_id),
-    CONSTRAINT user_fk4 FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    CONSTRAINT friend_fk FOREIGN KEY (friend_id) REFERENCES Users(user_id)
+    CONSTRAINT user_fk4 FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    CONSTRAINT friend_fk FOREIGN KEY (friend_id) REFERENCES Users(user_id) ON DELETE CASCADE
 ); #getting error with this table: duplicate foreign key constraint
 
+/*CREATE ASSERTION Comment-Constraint CHECK(
+	NOT EXISTS ( SELECT * FROM Comments C, Pictures P
+		WHERE C.photo_id = P.photo_id AND P.user_id = C.user_id));*/
 
 INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
 INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
